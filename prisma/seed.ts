63ee28cdeeacf5a3f96052e92a34f50b7775d3c3
machina20 <ryanschwa2@gmail.com>
2023-08-09 05:43:02 -0700

    able to query a db and display to user

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function main(){
+  const user = await prisma.user.upsert({
+    where: {
+      email: 'test@test.com'
+    },
+    update:{},
+    create:{
+      email: 'test@test.com',
+      name: 'Test User',
+      password: 'password'
+    }
+
+  })
+
+  console.log(user)
+}
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
