9714cb080565c053357fe86f78f9368c100c82d3
machina20 <64813073+machina20@users.noreply.github.com>
2023-08-12 05:19:28 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import { prisma } from "../../lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { User } from "./user";
+import { LoginButton, LogoutButton } from "./auth";
 
 export default async function Home() {
-	const user = await prisma.user.findFirst({
-		where: {
-			email: "test@test.com",
-		},
-	});
-	return <main className="flex justify-center pt-16">Hello {user?.name}</main>;
+	const session = await getServerSession(authOptions);
+
+	return (
+		<main className="bg-slate-300 h-screen">
+			<h1 className="font-bold text-xl">
+				getServerSession(authOptions) on server component
+			</h1>
+
+			<pre>{JSON.stringify(session)}</pre>
+			<h1 className="font-bold text-xl">useSession() on client component</h1>
+			<User></User>
+			<br></br>
+			<br></br>
+			<LoginButton></LoginButton>
+			<br></br>
+			<br></br>
+			<LogoutButton></LogoutButton>
+		</main>
+	);
 }
